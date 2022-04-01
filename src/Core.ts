c9298a7f47915fb411461f18ea916d301fcca58c
Kael Shipman <kael.shipman@gmail.com>
2022-03-31 20:02:50 -0500

    Many additions
    * Core.MatchingRule - A definition for a matching rule
    * Core.SplitTemplate - A split template
    * Core.Selection - A selection of transactions
    * Core.Api.Endpoints - A complex object used to make API Client typing
    more convenient and more robust.

--- src/Core.ts
+++ src/Core.ts
@@ -1,26 +1,80 @@
 import { Api as ApiTypes } from "@wymp/types";
 
-export type TemplateTransaction = {
-  /** The description to use for the created transaction */
-  description: string;
-
-  /** An array of tags to apply to the created transaction */
-  tags: Array<string>;
-
-  /** An array of splits that define the created transaction */
-  splits: Array<{
-    /** The virtual account id of this split */
-    virtualAccountId: string;
-
-    /**
-     * The percentage of the total amount that this split represents (100% == 100). The total of
-     * all splits of a given sign (+/-) should add up to 100, and the sum of all splits should be
-     * 0.
-     */
-    amountPerc: number;
-  }>;
+/**
+ * A matching rule. The match score of a rule is calculated against a given transaction by
+ * calculating the match percentage of each individual given component (date, desc or price),
+ * summing those numbers, then dividing them by the 10,000 times the number of components present.
+ * This gives us a percentage (0 to 100) indicating the final match score for the given rule.
+ */
+export type MatchingRule = {
+  date?:
+    | {
+        t: "proximal-date";
+        centerMs: number;
+        radiusDays: number;
+      }
+    | {
+        t: "date-range";
+        from: number;
+        to: number;
+      };
+  desc?: {
+    term: string;
+    regexp: boolean;
+  };
+  price?: {
+    centerBaseUnits: number;
+    radiusBaseUnits: number;
+  };
+  template: {
+    splits: Array<SplitTemplate>;
+    tags?: Array<string>;
+  };
 };
 
+/**
+ * A split template indicating various parameters of a split
+ */
+export type SplitTemplate = {
+  /** The id of the virtual account for this split */
+  virtualAccountId: string;
+  /** Whether this split is a credit or a debit against the given virtual account */
+  side: "credit" | "debit";
+  /**
+   * If unit is "%", a value from 0 to 100 indicating what percent of the total value of the tx this
+   * split should consume. If unit is "$", a monetary value in base units. If "rest", this split
+   * will take up whatever is left over after other splits have been calculated. Note that there
+   * can be multiple "rest" splits.
+   */
+  value: number | "rest";
+  /** Whether "value" is a percentage (0-100) or a monetary value in base units */
+  unit: "%" | "$";
+};
+
+/**
+ * A selection of transactions. This may be either a set of search criteria or a concrete set of
+ * transaction ids.
+ */
+export type Selection =
+  | {
+      t: "search";
+      date?: {
+        from: number;
+        to: number;
+      };
+      desc?: {
+        term: string;
+        regexp: boolean;
+      };
+    }
+  | {
+      t: "set";
+      ids: Array<string>;
+    };
+
+/**
+ * Attributes for the API data model. These are used in the database data model as well.
+ */
 export namespace Attributes {
   export type CashAccount = {
     type: "debit-accounts" | "credit-accounts";
@@ -36,9 +90,7 @@ export namespace Attributes {
   };
 
   export type MatchingRule = {
-    type: "description" | "counterparty" | "date-amount";
-    exactMatch: number;
-    valueJson: string;
+    value: MatchingRule;
   };
 
   export type Membership = {
@@ -185,7 +237,7 @@ export namespace Api {
   export type MatchingRule = Attributes.MatchingRule & {
     id: string;
     type: "matching-rules";
-    target: { data: { id: string; type: "recurring-transactions" | "virtual-accounts" | "tags" } };
+    cashAccount: ApiTypes.ToOneRelationship<"cash-accounts">;
   };
 
   export type Membership = Attributes.Membership & {
@@ -257,7 +309,7 @@ export namespace Api {
     id: string;
     type: "transactions";
     cashAccount: ApiTypes.ToOneRelationship<"cash-accounts">;
-    recurringTransaction: ApiTypes.ToOneRelationship<"cash-accounts", "nullable">;
+    recurringTransaction: ApiTypes.ToOneRelationship<"recurring-transactions", "nullable">;
   };
 
   export type Resource =
@@ -273,4 +325,133 @@ export namespace Api {
     | User
     | VirtualAccount
     | Transaction;
+
+  /**
+   * A namespace containing the concrete transactional data for API endpoints. Note that this
+   * describes the "data" value, NOT the entire request/response body. This is because the
+   * luminous client uses types that operate at the "data" level.
+   *
+   * For these definitions, "tx" indicates the shape of the object _transmitted_ to the endpoint,
+   * "rx" indicates the shape of the object _received_ from the endpoint, and "filter" indicates
+   * possible filter values that can be passed to the endpoint.
+   */
+  export type Endpoints = {
+    ["POST /cash-accounts"]: {
+      tx: {
+        type: "debit-accounts" | "credit-accounts";
+        name: string;
+        accountNum: string;
+        currencyId: "USD";
+        bankingProviderId: string;
+        owner?: {
+          type: "users";
+          id: string;
+        };
+      };
+      rx: CashAccount;
+    };
+
+    ["PATCH /cash-accounts/:id"]: {
+      tx: {
+        type: "cash-accounts";
+        name: string;
+      };
+      rx: CashAccount;
+    };
+
+    ["POST /spaces"]: {
+      tx: {
+        type: "spaces";
+        name?: string | null;
+      };
+      rx: Space;
+    };
+
+    /**
+     * NOTE: This endpoint is intended to be used internally by the gateway service. Therefore
+     * it requires an id.
+     */
+    ["POST /users"]: {
+      tx: {
+        type: "users";
+        id: string;
+        name: string;
+        email: string;
+      };
+    };
+
+    ["POST /virtual-accounts"]: {
+      tx:
+        | {
+            type: "budget-accounts";
+            name: string;
+            targetAmountBaseUnits?: number | null;
+          }
+        | {
+            type: "goal-accounts";
+            name: string;
+            targetAmountBaseUnits?: number | null;
+            targetDateMs?: number | null;
+          };
+      rx: VirtualAccount;
+    };
+
+    /**
+     * NOTE: This endpoint is generally not intended to be used for "external" transactions, i.e.,
+     * money moving into and out of a given cash account. It is more intended for internal
+     * transactions.
+     */
+    ["POST /transactions"]: {
+      tx: {
+        type: "transactions";
+        status: "expected" | "cleared";
+        description?: string;
+        timestampMs?: number;
+        amountBaseUnits: number;
+        balanceBaseUnits?: number;
+        splits: Array<{
+          virtualAccountId: string;
+          amountBaseUnits: number;
+        }>;
+      };
+      rx: Transaction;
+    };
+
+    ["GET /transactions"]: {
+      filter: {
+        date?: {
+          from: number;
+          to: number;
+        };
+        desc?: {
+          term: string;
+          regexp: boolean;
+        };
+      };
+    };
+
+    ["PATCH /transactions/:id"]: {
+      tx: {
+        type: "transactions";
+        description?: string;
+        splits?: Array<SplitTemplate>;
+      };
+      rx: Transaction;
+    };
+
+    ["POST /transactions/bulk-edit"]: {
+      tx: {
+        transaction: {
+          type: "transactions";
+          description?: string;
+          splits?: Array<SplitTemplate>;
+        };
+        selection: Selection;
+      };
+      rx: {
+        type: "warnings";
+        warnings: Array<string>;
+      };
+    };
+  };
 }
